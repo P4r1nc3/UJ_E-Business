0294e141abc9f0b5ec1aaa18ed79949a06c20743
parince <konrad.tupta@gmail.com>
2024-03-20 18:12:07 +0100

    Remade POST and PUT /products/{productId} request bodies

--- exercise02-scala/app/controllers/ProductController.scala
+++ exercise02-scala/app/controllers/ProductController.scala
@@ -9,6 +9,7 @@ import play.api.libs.json.{Json, OFormat}
 @Singleton
 class ProductController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   private val products = ListBuffer[Product]()
+  private var nextId: Long = 1
   implicit val productFormat: OFormat[Product] = Json.format[Product]
 
   def list(): Action[AnyContent] = Action {
@@ -23,20 +24,31 @@ class ProductController @Inject()(cc: ControllerComponents) extends AbstractCont
   }
 
   def create(): Action[AnyContent] = Action { request =>
-    val json = request.body.asJson.get
-    val product = json.as[Product]
-    products += product
-    Created(s"Product created with id: ${product.id}")
+    request.body.asJson match {
+      case Some(json) =>
+        val name = (json \ "name").as[String]
+        val categoryId = (json \ "categoryId").as[Long]
+        val newProduct = Product(id = nextId, name = name, categoryId = categoryId)
+        products += newProduct
+        nextId += 1
+        Created(Json.toJson(newProduct))
+      case None => BadRequest("Invalid product data")
+    }
   }
 
   def update(id: Long): Action[AnyContent] = Action { request =>
-    val json = request.body.asJson.get
-    val newProduct = json.as[Product]
-    products.indexWhere(_.id == id) match {
-      case idx if idx != -1 =>
-        products.update(idx, newProduct)
-        Ok(s"Product with id $id updated.")
-      case _ => NotFound
+    request.body.asJson match {
+      case Some(json) =>
+        val name = (json \ "name").as[String]
+        val categoryId = (json \ "categoryId").as[Long]
+        products.indexWhere(_.id == id) match {
+          case idx if idx != -1 =>
+            val updatedProduct = products(idx).copy(name = name, categoryId = categoryId)
+            products.update(idx, updatedProduct)
+            Ok(Json.toJson(updatedProduct))
+          case _ => NotFound
+        }
+      case None => BadRequest("Invalid product data")
     }
   }
 
