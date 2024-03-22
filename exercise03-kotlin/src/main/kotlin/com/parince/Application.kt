0d3d8eea524c9076151fd280a750f83ac7d54e81
parince <konrad.tupta@gmail.com>
2024-03-22 14:40:53 +0100

    Made sending messages per endpoints

--- exercise03-kotlin/src/main/kotlin/com/parince/Application.kt
+++ exercise03-kotlin/src/main/kotlin/com/parince/Application.kt
@@ -1,27 +1,51 @@
 package com.parince
 
 import io.ktor.client.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.util.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.util.*
+import io.ktor.server.routing.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import kotlinx.serialization.Serializable
 
-suspend fun main() {
+@Serializable
+data class MessagePost(val message: String)
+
+fun main() {
+    val server = embeddedServer(Netty, port = 8080) {
+        install(ContentNegotiation) {
+            json()
+        }
+        module()
+    }
+    server.start(wait = true)
+}
+
+fun Application.module() {
     val client = HttpClient(CIO)
+    routing {
+        post("/discord/{channelId}") {
+            val channelId = call.parameters["channelId"] ?: return@post call.respond(HttpStatusCode.BadRequest)
+            val postParameters = call.receive<MessagePost>()
+            sendToDiscord(client, channelId, "MTE3ODQ0NjUwMDgxNzI4NTEyMA.GlutqJ.R6dNwrqYdlDP4xd1xSLNCBcs24pocN4y74NZMA", postParameters.message)
+            call.respondText("Message sent to Discord channel $channelId", status = HttpStatusCode.OK)
+        }
 
-    val discordChannelId = "1220501870766723202"
-    val discordToken = "MTE3ODQ0NjUwMDgxNzI4NTEyMA.GlutqJ.R6dNwrqYdlDP4xd1xSLNCBcs24pocN4y74NZMA"
-
-    val slackChannelId = "C06QL4U3FKQ"
-    val slackToken = "xoxb-6845263267716-6828282409719-oxiJDWmbk0SJvIg92KqtQyAb"
-
-    val messageContent = "Hello World!"
-
-    sendToDiscord(client, discordChannelId, discordToken, messageContent)
-    sendToSlack(client, slackChannelId, slackToken, messageContent)
-
-    client.close()
+        post("/slack/{channelId}") {
+            val channelId = call.parameters["channelId"] ?: return@post call.respond(HttpStatusCode.BadRequest)
+            val postParameters = call.receive<MessagePost>()
+            sendToSlack(client, channelId, "xoxb-6845263267716-6828282409719-oxiJDWmbk0SJvIg92KqtQyAb", postParameters.message)
+            call.respondText("Message sent to Slack channel $channelId", status = HttpStatusCode.OK)
+        }
+    }
 }
 
 @OptIn(InternalAPI::class)
