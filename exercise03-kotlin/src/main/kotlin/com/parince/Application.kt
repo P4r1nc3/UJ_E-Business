97bb9c2060e092267629be74d93dd6b0b085fe69
parince <konrad.tupta@gmail.com>
2024-03-22 08:41:25 +0100

    Created sendToDiscord function

--- exercise03-kotlin/src/main/kotlin/com/parince/Application.kt
+++ exercise03-kotlin/src/main/kotlin/com/parince/Application.kt
@@ -1,15 +1,42 @@
 package com.parince
 
-import com.parince.plugins.*
-import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.util.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+suspend fun main() {
+    val client = HttpClient(CIO)
+
+    val discordChannelId = "1220501870766723202"
+    val discordToken = "MTE3ODQ0NjUwMDgxNzI4NTEyMA.GlutqJ.R6dNwrqYdlDP4xd1xSLNCBcs24pocN4y74NZMA"
+    val messageContent = "Hello World!"
+
+    sendToDiscord(client, discordChannelId, discordToken, messageContent)
+    client.close()
 }
 
-fun Application.module() {
-    configureRouting()
+@OptIn(InternalAPI::class)
+suspend fun sendToDiscord(client: HttpClient, channelId: String, token: String, messageContent: String) {
+    try {
+        val response: HttpResponse = client.post("https://discord.com/api/v10/channels/$channelId/messages") {
+            header(HttpHeaders.Authorization, "Bot $token")
+            header(HttpHeaders.ContentType, ContentType.Application.Json)
+            body = """
+                {
+                    "content": "$messageContent"
+                }
+            """.trimIndent()
+        }
+
+        if (response.status == HttpStatusCode.OK) {
+            println("Message has been sent successfully to Discord.")
+        } else {
+            println("Failed to send message to Discord. Status: ${response.status}")
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
