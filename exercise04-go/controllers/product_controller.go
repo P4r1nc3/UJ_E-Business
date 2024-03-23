b2aac7470f211855b3b95b8327e5e1711a146238
parince <konrad.tupta@gmail.com>
2024-03-23 22:21:46 +0100

    Implemented CRUD operations inside product_controller

--- exercise04-go/controllers/product_controller.go
+++ exercise04-go/controllers/product_controller.go
@@ -16,3 +16,46 @@ func CreateProduct(c echo.Context) error {
 	db.Create(&product)
 	return c.JSON(http.StatusCreated, product)
 }
+
+func GetProducts(c echo.Context) error {
+	db := c.Get("db").(*gorm.DB)
+	var products []models.Product
+	db.Find(&products)
+	return c.JSON(http.StatusOK, products)
+}
+
+func GetProduct(c echo.Context) error {
+	id := c.Param("id")
+	db := c.Get("db").(*gorm.DB)
+	var product models.Product
+	result := db.First(&product, id)
+	if result.Error != nil {
+		return c.JSON(http.StatusNotFound, "Product not found")
+	}
+	return c.JSON(http.StatusOK, product)
+}
+
+func UpdateProduct(c echo.Context) error {
+	id := c.Param("id")
+	db := c.Get("db").(*gorm.DB)
+	var product models.Product
+	if db.First(&product, id).Error != nil {
+		return c.JSON(http.StatusNotFound, "Product not found")
+	}
+	if err := c.Bind(&product); err != nil {
+		return err
+	}
+	db.Save(&product)
+	return c.JSON(http.StatusOK, product)
+}
+
+func DeleteProduct(c echo.Context) error {
+	id := c.Param("id")
+	db := c.Get("db").(*gorm.DB)
+	var product models.Product
+	if db.First(&product, id).Error != nil {
+		return c.JSON(http.StatusNotFound, "Product not found")
+	}
+	db.Delete(&product)
+	return c.NoContent(http.StatusNoContent)
+}
