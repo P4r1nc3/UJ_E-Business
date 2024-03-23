d82e953c0e1efc2fb9bd826f14a1e0882fc33e1b
parince <konrad.tupta@gmail.com>
2024-03-23 22:08:42 +0100

    Made db setup

--- exercise04-go/server.go
+++ exercise04-go/server.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"net/http"
+	"goproject/controllers"
+	"goproject/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+
+	dsn := "root:admin12345@tcp(localhost:3306)/goproject?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Set("db", db)
+			return next(c)
+		}
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	db.AutoMigrate(&models.Product{})
+
+	e.POST("/products", controllers.CreateProduct)
+	e.Start(":8080")
 }
