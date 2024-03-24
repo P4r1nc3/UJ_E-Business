4313b04ef964c577f1b3eb15214e60f151846750
parince <konrad.tupta@gmail.com>
2024-03-24 19:30:27 +0100

    Created cart_router

---
+++ exercise05-frontend/backend/routes/cart_router.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/labstack/echo/v4"
+	"gobackend/handlers"
+)
+
+func CartRoutes(e *echo.Echo) {
+	e.POST("/carts", handlers.CreateCart)
+	e.GET("/carts/:cartId", handlers.GetCart)
+	e.DELETE("/carts/:cartId", handlers.DeleteCart)
+	e.POST("/carts/:cartId/products/:productId", handlers.AddProductToCart)
+	e.DELETE("/carts/:cartId/products/:productId", handlers.DeleteProductFromCart)
+}
