50ac1c76e94f700534b444c949d4f079d196d1de
parince <konrad.tupta@gmail.com>
2024-03-26 20:48:17 +0100

    Created CORS configuration

--- exercise05-frontend/backend/server.go
+++ exercise05-frontend/backend/server.go
@@ -3,10 +3,18 @@ package main
 import (
 	"gobackend/database"
 	"gobackend/routes"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := database.SetupEcho()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	}))
 
 	routes.ProductRoutes(e)
 	routes.CartRoutes(e)
