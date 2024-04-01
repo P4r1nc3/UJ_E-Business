9f0827445fe8858bb0b74cc96e1ec08f0c56f4dc
P4r1nc3 <konrad.tupta@gmail.com>
2024-04-01 13:27:53 +0200

    Created simple cors config on backend side

--- exercise08-oauth2/backend/app.js
+++ exercise08-oauth2/backend/app.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Połączono z bazą danych"))
+    .then(() => console.log("Successfully connected with MongoDB"))
     .catch((err) => console.error(err));
 
 app.use('/api/user', authRoutes);
