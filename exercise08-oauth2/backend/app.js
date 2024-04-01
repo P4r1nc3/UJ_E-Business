9d836ff813997eb9a84acac4c9412390abd89bfe
P4r1nc3 <konrad.tupta@gmail.com>
2024-04-01 13:07:54 +0200

    Updated app.js

--- exercise08-oauth2/backend/app.js
+++ exercise08-oauth2/backend/app.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Połączono z bazą danych"))
+    .catch((err) => console.error(err));
+
+app.use('/api/user', authRoutes);
+
 const PORT = process.env.PORT || 3001;
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
