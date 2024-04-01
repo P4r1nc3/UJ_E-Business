1797af337668c5cdee07d573e85880c380bfee90
P4r1nc3 <konrad.tupta@gmail.com>
2024-04-01 20:14:21 +0200

    Styled Login form with tailwind css

--- exercise08-oauth2/frontend/src/components/Login.js
+++ exercise08-oauth2/frontend/src/components/Login.js
@@ -14,18 +14,35 @@ const Login = () => {
             localStorage.setItem('userToken', data);
             navigate("/dashboard");
         } catch (error) {
-            alert('Login failed: ', error.response.data);
+            alert('Login failed: ' + error.response.data);
         }
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required />
-                <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
-                <button type="submit">Login</button>
-            </form>
+        <div style={{ minHeight: 'calc(100vh - 4rem)' }} className="flex justify-center items-center bg-gray-100">
+            <div className="w-full max-w-lg bg-white shadow-md rounded px-10 pt-8 pb-10 mb-4">
+                <h2 className="mb-8 text-center text-3xl font-extrabold text-gray-900">Login</h2>
+                <form onSubmit={handleSubmit} className="space-y-6">
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">Email</label>
+                        <input type="email" placeholder="Email" value={email}
+                               onChange={e => setEmail(e.target.value)}
+                               className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                               required />
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">Password</label>
+                        <input type="password" placeholder="Password" value={password}
+                               onChange={e => setPassword(e.target.value)}
+                               className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                               required />
+                    </div>
+                    <button type="submit"
+                            className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Login
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
