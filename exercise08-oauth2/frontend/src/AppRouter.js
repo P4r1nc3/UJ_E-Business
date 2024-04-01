8a203b46c66a868435e4e149ecf785e864db5d54
P4r1nc3 <konrad.tupta@gmail.com>
2024-04-01 13:39:56 +0200

    Updated AppRouter

--- exercise08-oauth2/frontend/src/AppRouter.js
+++ exercise08-oauth2/frontend/src/AppRouter.js
@@ -1,13 +1,16 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Register from './components/Register';
+import RequireAuth from './helpers/RequireAuth';
 
 const AppRouter = () => (
     <Router>
         <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} />
+            {/* Dodaj inne ścieżki tutaj */}
         </Routes>
     </Router>
 );
