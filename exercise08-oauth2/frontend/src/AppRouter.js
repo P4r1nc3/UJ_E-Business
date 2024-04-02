9e0d8541838cf8fcfb97a4ed076372cacff01547
P4r1nc3 <konrad.tupta@gmail.com>
2024-04-02 19:53:43 +0200

    Updated Login component

--- exercise08-oauth2/frontend/src/AppRouter.js
+++ exercise08-oauth2/frontend/src/AppRouter.js
@@ -11,7 +11,7 @@ const AppRouter = () => (
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} />
+            <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
     </Router>
 );
--- exercise08-oauth2/frontend/src/components/Login.js
+++ exercise08-oauth2/frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -40,6 +41,15 @@ const Login = () => {
                     <button type="submit"
                             className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         Login
+                    </button>
+                    <button
+                        onClick={() => window.location.href='http://localhost:3001/api/user/auth/google'}
+                        className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      <span className="mr-2">
+                        <FcGoogle size={20} />
+                      </span>
+                        Login with Google
                     </button>
                 </form>
             </div>
