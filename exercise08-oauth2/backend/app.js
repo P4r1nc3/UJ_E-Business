da84c7ea3168e9f87caecb06268001ae0abacb7d
P4r1nc3 <konrad.tupta@gmail.com>
2024-04-02 19:31:14 +0200

    Adjusted app.js

--- exercise08-oauth2/backend/app.js
+++ exercise08-oauth2/backend/app.js
@@ -2,16 +2,32 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const session = require('express-session');
 const authRoutes = require('./routes/authRoutes');
+const passport = require('passport');
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:3000',
+    credentials: true
 }));
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Successfully connected with MongoDB"))
