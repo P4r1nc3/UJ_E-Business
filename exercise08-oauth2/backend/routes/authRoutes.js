fdb93febea9aeec07472fd6ff0a2a99daa095440
P4r1nc3 <konrad.tupta@gmail.com>
2024-04-02 19:30:10 +0200

    Added Google Oauth2 routes

--- exercise08-oauth2/backend/routes/authRoutes.js
+++ exercise08-oauth2/backend/routes/authRoutes.js
@@ -2,8 +2,40 @@ const express = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+require('dotenv').config();
 
 const router = express.Router();
+
+
+passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/api/user/google/callback"
+    },
+    async (accessToken, refreshToken, profile, done) => {
+        try {
+            let user = await User.findOne({ googleId: profile.id });
+            if (!user) {
+                user = await User.create({
+                    googleId: profile.id,
+                    email: profile.emails[0].value,
+                    isLocal: false
+                });
+            }
+            done(null, user);
+        } catch (error) {
+            done(error, null);
+        }
+    }
+));
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => done(null, user));
+});
 
 router.post('/register', async (req, res) => {
     try {
@@ -36,5 +68,17 @@ router.post('/login', async (req, res) => {
         res.status(500).send(err.message);
     }
 });
+
+router.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+        const token = jwt.sign({ _id: req.user._id }, process.env.TOKEN_SECRET);
+        res.redirect(`http://localhost:3000/?token=${token}`);
+    }
+);
 
 module.exports = router;
