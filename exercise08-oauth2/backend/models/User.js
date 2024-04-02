0c7b470dd1e5658ddb8982199bd8a6adf8d49469
P4r1nc3 <konrad.tupta@gmail.com>
2024-04-02 19:30:34 +0200

    Updated User model

--- exercise08-oauth2/backend/models/User.js
+++ exercise08-oauth2/backend/models/User.js
@@ -3,14 +3,24 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: function() { return this.isLocal; }},
+    isLocal: { type: Boolean, default: true },
+    googleId: { type: String, required: false }
 });
 
 userSchema.pre('save', async function (next) {
-    if (this.isModified('password') || this.isNew) {
-        this.password = await bcrypt.hash(this.password, 10);
+    if (this.isModified('password') && this.password) {
+        try {
+            const hashedPassword = await bcrypt.hash(this.password, 10);
+            this.password = hashedPassword;
+            next();
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        next();
     }
-    next();
 });
+
 
 module.exports = mongoose.model('User', userSchema);
