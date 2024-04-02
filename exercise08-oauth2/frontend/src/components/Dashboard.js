d20f03bf6e4aace1e44709fa143b0a787626e648
P4r1nc3 <konrad.tupta@gmail.com>
2024-04-02 20:08:32 +0200

    Made Dashboard accessible only for authenticated users

--- exercise08-oauth2/frontend/src/components/Dashboard.js
+++ exercise08-oauth2/frontend/src/components/Dashboard.js
@@ -7,12 +7,17 @@ const Dashboard = () => {
 
     useEffect(() => {
         const params = new URLSearchParams(location.search);
-        const token = params.get('token');
-        if (token) {
-            localStorage.setItem('userToken', token);
+        const tokenFromURL = params.get('token');
+        const tokenInStorage = localStorage.getItem('userToken');
+
+        if (tokenFromURL) {
+            localStorage.setItem('userToken', tokenFromURL);
             navigate('/dashboard', { replace: true });
+        } else if (!tokenInStorage) {
+            navigate('/login');
         }
     }, [location, navigate]);
+
     return (
         <div className="flex justify-center items-center bg-gray-100" style={{ minHeight: 'calc(100vh - 4rem)' }}>
             <h2 className="text-2xl font-bold text-gray-900">You are logged in</h2>
