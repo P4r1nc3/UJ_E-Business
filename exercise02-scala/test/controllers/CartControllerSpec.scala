08a1206a924b5f9119993457e6f3a70386d0a956
parince <konrad.tupta@gmail.com>
2024-04-11 21:08:42 +0200

    Added additional asserts inside CartControllerSpec

--- exercise02-scala/test/controllers/CartControllerSpec.scala
+++ exercise02-scala/test/controllers/CartControllerSpec.scala
@@ -5,17 +5,18 @@ import org.scalatestplus.mockito.MockitoSugar
 import play.api.mvc._
 import play.api.test._
 import play.api.test.Helpers._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, Json}
 
 class CartControllerSpec extends PlaySpec with Results with MockitoSugar {
 
   val mockControllerComponents = Helpers.stubControllerComponents()
 
   "CartController#list" should {
-    "return an OK status with a JSON list of carts" in {
+    "return an OK status with a JSON list of carts that verifies the cart details" in {
       // Arrange
       val cartController = new CartController(mockControllerComponents)
       CartController.carts.clear()
+      CartController.carts += Cart(1, List(CartItem(1, 2)))
 
       // Act
       val result = cartController.list().apply(FakeRequest(GET, "/carts"))
@@ -23,16 +24,20 @@ class CartControllerSpec extends PlaySpec with Results with MockitoSugar {
       // Assert
       status(result) mustBe OK
       contentType(result) mustBe Some("application/json")
+      val json: JsValue = contentAsJson(result)
+      (json \ 0 \ "id").as[Long] mustBe 1
+      (json \ 0 \ "items" \ 0 \ "productId").as[Long] mustBe 1
+      (json \ 0 \ "items" \ 0 \ "quantity").as[Int] mustBe 2
     }
   }
 
   "CartController#get" should {
-    "return an OK status with the cart JSON for an existing cart ID" in {
+    "return an OK status with the cart JSON for an existing cart ID and verify the cart contents" in {
       // Arrange
       val cartId = 1L
       val cartController = new CartController(mockControllerComponents)
       CartController.carts.clear()
-      CartController.carts += Cart(cartId, List(CartItem(1, 2))) // Corrected
+      CartController.carts += Cart(cartId, List(CartItem(1, 2)))
 
       // Act
       val result = cartController.get(cartId).apply(FakeRequest(GET, s"/carts/$cartId"))
@@ -40,11 +45,15 @@ class CartControllerSpec extends PlaySpec with Results with MockitoSugar {
       // Assert
       status(result) mustBe OK
       contentType(result) mustBe Some("application/json")
+      val cartJson: JsValue = contentAsJson(result)
+      (cartJson \ "id").as[Long] mustBe cartId
+      (cartJson \ "items" \ 0 \ "productId").as[Long] mustBe 1
+      (cartJson \ "items" \ 0 \ "quantity").as[Int] mustBe 2
     }
   }
 
   "CartController#create" should {
-    "create a new cart and return a Created status with the cart JSON" in {
+    "create a new cart and return a Created status with the cart JSON verifying the cart structure and content" in {
       // Arrange
       val cartController = new CartController(mockControllerComponents)
       CartController.carts.clear()
@@ -64,16 +73,20 @@ class CartControllerSpec extends PlaySpec with Results with MockitoSugar {
       // Assert
       status(result) mustBe CREATED
       contentType(result) mustBe Some("application/json")
+      val cartJson: JsValue = contentAsJson(result)
+      (cartJson \ "id").as[Long] mustBe 1
+      (cartJson \ "items" \ 0 \ "productId").as[Long] mustBe 1
+      (cartJson \ "items" \ 0 \ "quantity").as[Int] mustBe 2
     }
   }
 
   "CartController#update" should {
-    "update an existing cart and return an OK status with the updated cart JSON" in {
+    "update an existing cart and return an OK status with the updated cart JSON verifying changes" in {
       // Arrange
       val cartId = 2L
       val cartController = new CartController(mockControllerComponents)
       CartController.carts.clear()
-      CartController.carts += Cart(cartId, List(CartItem(1, 1))) // Corrected
+      CartController.carts += Cart(cartId, List(CartItem(1, 1)))
       ProductController.products += Product(1, 1, "Another Product", "Another product for testing", 49.99)
       val updateJson = Json.obj(
         "items" -> Json.arr(
@@ -87,22 +100,25 @@ class CartControllerSpec extends PlaySpec with Results with MockitoSugar {
       // Assert
       status(result) mustBe OK
       contentType(result) mustBe Some("application/json")
+      val updatedCartJson: JsValue = contentAsJson(result)
+      (updatedCartJson \ "items" \ 0 \ "quantity").as[Int] mustBe 3
     }
   }
 
   "CartController#delete" should {
-    "delete an existing cart and return a NoContent status" in {
+    "delete an existing cart and return a NoContent status while ensuring the cart is removed from the list" in {
       // Arrange
       val cartId = 3L
       val cartController = new CartController(mockControllerComponents)
       CartController.carts.clear()
-      CartController.carts += Cart(cartId, List(CartItem(1, 2))) // Corrected
+      CartController.carts += Cart(cartId, List(CartItem(1, 2)))
 
       // Act
       val result = cartController.delete(cartId).apply(FakeRequest(DELETE, s"/carts/$cartId"))
 
       // Assert
       status(result) mustBe NO_CONTENT
+      CartController.carts.exists(_.id == cartId) mustBe false
     }
   }
 }
